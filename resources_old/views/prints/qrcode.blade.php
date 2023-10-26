2a157249482e7406d63949018b957159947c8dcd
JeremiasStoll <yuki.stoll@proton.me>
2023-10-26 12:56:56 +0200

    deleted unneeded files

--- resources_old/views/prints/qrcode.blade.php
+++
@@ -1,7 +0,0 @@
-<html>
-    <body onload="window.print()">
-        <p>{{ $visitor }}</p>
-        <img src="data:image/png;base64,{!! base64_encode(QrCode::format('png')->size(100)->generate($id . '#')) !!}">
-        <p>{{ $id }}</p>
-    </body>
-</html>
